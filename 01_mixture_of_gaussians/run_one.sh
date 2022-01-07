a579829fe2ee889274940250e88dbd6ef36eada1
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-01-06 21:57:31 -0800

    Polished sweep for mixture of gaussians.

--- 01_mixture_of_gaussians/run_one.sh
+++ 01_mixture_of_gaussians/run_one.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p fiete
 #SBATCH -n 2                    # two cores
-#SBATCH --mem=24G               # RAM
+#SBATCH --mem=16G               # RAM
 #SBATCH --time=99:99:99         # total run time limit (HH:MM:SS)
 #SBATCH --mail-user=rylansch
 #SBATCH --mail-type=FAIL
@@ -13,7 +13,7 @@ export PYTHONPATH=.
 export WANDB_CONFIG_DIR=/om2/user/rylansch
 export WANDB_API_KEY=51a0a43a1b4ba9981701d60c5f6887cd5bf9e03e
 
-source dpmeanspp_venv/bin/activate
+source rncrp_venv/bin/activate
 
 # write the executed command to the slurm output file for easy reproduction
 # https://stackoverflow.com/questions/5750450/how-can-i-print-each-command-before-executing
