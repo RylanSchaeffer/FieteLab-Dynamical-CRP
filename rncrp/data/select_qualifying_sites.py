5f05b89c55cda8f81efa168d51a5cdcfbb538ffd
Gabrielle 'Kaili' Liu <pybeebee@gmail.com>
2022-01-09 03:34:50 -0500

    Fixed merge issues

--- rncrp/data/select_qualifying_sites.py
+++ rncrp/data/select_qualifying_sites.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
 #################################################################
-# Obtain list of sites with data from 1946 to 2020
+# Obtain list of sites with data from 1946 to END_YEAR
 #################################################################
-def satisfy_dates(inventory_loc: str = "./ghcnd-inventory.txt"):
+def satisfy_dates(inventory_loc: str = "/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/ghcnd-inventory.txt",
+                  end_year: int = 2020):
     sites = pd.read_csv(inventory_loc,sep='\s+',header=None)
     sites.columns = ["station","lat","long","field","start","end"]
 
-    sites = sites[(sites.start<=1946) & (sites.end>=2020)]
+    sites = sites[(sites.start<=1946) & (sites.end>=end_year)]
     sites_to_fetch = sites.station.unique().tolist()
+    print("Number of sites satisfying date range:",len(sites_to_fetch))
 
-    with open("sites_with_valid_dates.txt","w+") as f:
+    with open("/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/sites_with_valid_dates_"+str(end_year)+".txt","w+") as f:
         for item in sites_to_fetch:
             f.write(item+"\n")
     f.close()
-satisfy_dates()
 
 #################################################################
 # Filter for stations with sufficient data (tmin, tmax, pcpn)
@@ -30,7 +31,7 @@ def qualify_checker(df):
     df['YEAR'] = df.DATE.apply(lambda x: int(x[:4]))
     df['MONTH'] = df.DATE.apply(lambda x: int(x[5:7]))
 
-    for year in range(1946,2021):
+    for year in range(1946,end_year+1):
         for month in range(1,13):
             # At least 90% data present per month (and thus per year)
             if df[(df.YEAR==year) & (df.MONTH==month)].TMIN.count().sum() < min_required_days[month]:
@@ -48,11 +49,13 @@ def qualify_checker(df):
                 return False
     return True
 
-def get_qualifying_sites():
+def get_qualifying_sites(end_year: int = 2020):
     qualifying_sites = []
-    with open('./sites_with_valid_dates.txt') as file:
+    qualifying_site_links = []
+    with open('/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/sites_with_valid_dates_'+str(end_year)+'.txt') as file:
         for site_name in file:
             site_csv_path = '/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/data/'+site_name.strip()+'.csv'
+            site_csv_link = 'https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/'+site_name.strip()+'.csv'
 
             # Load file into dataframe
             df = pd.read_csv(site_csv_path, low_memory=False)
@@ -61,11 +64,20 @@ def get_qualifying_sites():
             # If criteria satisfied
             if qualify_checker(df):
                 qualifying_sites.append(site_csv_path)
+                qualifying_site_links.append(site_csv_link)
                 print(site_name)
 
     print("Number of qualifying sites:",len(qualifying_sites))
-    with open("qualifying_sites.txt","w+") as f:
+    with open("/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/qualifying_sites_"+str(end_year)+".txt","w+") as f:
         for site_csv_path in qualifying_sites:
             f.write(site_csv_path+"\n")
     f.close()
-get_qualifying_sites()
+
+    with open("/om2/user/gkml/FieteLab-Recursive-Nonstationary-CRP/exp2_climate/qualifying_site_links_"+str(end_year)+".txt","w+") as f:
+        for site_csv_link in qualifying_site_links:
+            f.write(site_csv_link+"\n")
+    f.close()
+
+end_year = 2015
+# satisfy_dates(end_year=end_year)
+get_qualifying_sites(end_year)
