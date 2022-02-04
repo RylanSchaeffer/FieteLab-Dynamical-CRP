20a8827c4a01f993441b7fb0cd1e343f978a2922
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-02-04 10:42:42 -0800

    Created 03 mixture of von mises Fisher

---
+++ 03_mixture_of_vonmisesfisher/analyze_sweep.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+import pandas as pd
+
+from rncrp.helpers.run import download_wandb_project_runs_results
+import plot_mixture_of_vonmisesfisher
+
+exp_dir = '03_mixture_of_vonmisesfisher'
+results_dir = os.path.join(exp_dir, 'results')
+os.makedirs(results_dir, exist_ok=True)
+wandb_sweep_path = "rylan/dcrp-mixture-of-vonmisesfisher"
+sweep_name = '5vkotbim'
+sweep_dir = os.path.join(results_dir, sweep_name)
+os.makedirs(sweep_dir, exist_ok=True)
+sweep_results_df_path = os.path.join(sweep_dir, f'sweep={sweep_name}_results.csv')
+
+if not os.path.isfile(sweep_results_df_path):
+    sweep_results_df = download_wandb_project_runs_results(
+        wandb_project_path=wandb_sweep_path,
+        sweep_id=sweep_name)
+
+    # Compute SNR := rho / sigma
+    sweep_results_df['cov_prefactor_ratio'] = sweep_results_df['centroids_prior_cov_prefactor'] \
+                                              / sweep_results_df['likelihood_cov_prefactor']
+
+    sweep_results_df.to_csv(sweep_results_df_path, index=False)
+
+else:
+    sweep_results_df = pd.read_csv(sweep_results_df_path)
+
+
+plot_mixture_of_gaussians.plot_analyze_all_inf_algs_results(
+    all_inf_algs_results_df=sweep_results_df,
+    plot_dir=sweep_dir,
+)
+
+print(f'Finished 03_mixture_of_vonmisesfisher/plot_sweep.py with sweep={sweep_name}.')
--- 03_mixture_of_vonmisesfisher/run_one.py
+++ 03_mixture_of_vonmisesfisher/run_one.py
@@ -1,10 +1,10 @@
 """
-Perform inference in Ames Housing for the specified inference algorithm
-and model parameter.
+Perform inference in a synthetic mixture of von Mises-Fisher for the specified inference
+algorithm and model parameters.
 
 Example usage:
 
-02_ames_housing/run_one.py
+03_mixture_of_vonmisesfisher/run_one.py
 """
 
 import argparse
@@ -15,7 +15,8 @@ import os
 import torch
 import wandb
 
-import rncrp.data.real
+# import plot
+import rncrp.data.synthetic
 import rncrp.helpers.dynamics
 import rncrp.helpers.run
 import rncrp.metrics
@@ -24,11 +25,14 @@ config_defaults = {
     # 'inference_alg_str': 'VI-GMM',
     # 'inference_alg_str': 'DP-Means (Offline)',
     'inference_alg_str': 'Dynamical-CRP',
-    'dynamics_str': 'sinusoid',
+    'dynamics_str': 'hyperbolic',
     'dynamics_a': 1.,
     'dynamics_b': 1.,
     'dynamics_c': 1.,
     'dynamics_omega': np.pi / 2.,
+    'n_samples': 27,
+    'n_features': 10,
+    'n_clusters': 40,
     'alpha': 0.1,
     'beta': 0.,
     'centroids_prior_cov_prefactor': 5.,
@@ -36,7 +40,7 @@ config_defaults = {
     'repeat_idx': 0,
 }
 
-wandb.init(project='dcrp-ames-housing',
+wandb.init(project='dcrp-mixture-of-vonmisesfisher',
            config=config_defaults)
 config = wandb.config
 
@@ -44,8 +48,8 @@ print(f'Running:')
 for key, value in config.items():
     print(key, ' : ', value)
 
-# Set paths and create if necessary.
-exp_dir = '02_ames_housing'
+# determine paths
+exp_dir = '03_mixture_of_vonmisesfisher'
 results_dir_path = os.path.join(exp_dir, 'results')
 os.makedirs(results_dir_path, exist_ok=True)
 inf_alg_results_path = os.path.join(results_dir_path,
@@ -53,38 +57,26 @@ inf_alg_results_path = os.path.join(results_dir_path,
 wandb.log({'inf_alg_results_path': inf_alg_results_path},
           step=0)
 
-# Set seeds.
+# set seeds
 rncrp.helpers.run.set_seed(config['repeat_idx'])
 
-# Load data and permute order of data based on seed.
-ames_housing_data = rncrp.data.real.load_dataset_ames_housing_2011()
-n_obs = ames_housing_data['observations'].shape[0]
-permutation = np.random.permutation(np.arange(n_obs))
-ames_housing_data['observations'] = \
-    ames_housing_data['observations'][permutation]
-ames_housing_data['labels'] = \
-    ames_housing_data['labels'][permutation]
-print('Randomly permuted the order of 2016 Cancer Gene Expression data.')
-
-# Compute the correct parameters, depending on the dynamics
-if config['dynamics_str'] == 'step':
-    dynamics_params = {}
-elif config['dynamics_str'] == 'exp':
-    dynamics_params = {}
-elif config['dynamics_str'] == 'hyperbolic':
-    dynamics_params = {}
-elif config['dynamics_str'] == 'sinusoid':
-    dynamics_params = {}
-else:
-    raise NotImplementedError
-
+mixture_model_results = rncrp.data.synthetic.sample_mixture_model(
+    num_obs=config['n_samples'],
+    obs_dim=config['n_features'],
+    mixing_prior_str='rncrp',
+    mixing_distribution_params={'alpha': config['alpha'],
+                                'beta': config['beta'],
+                                'dynamics_str': config['dynamics_str']},
+    component_prior_str='gaussian',
+    component_prior_params={'centroids_prior_cov_prefactor': config['centroids_prior_cov_prefactor'],
+                            'likelihood_cov_prefactor': config['likelihood_cov_prefactor']})
 
 gen_model_params = {
     'mixing_params': {
         'alpha': config['alpha'],
         'beta': config['beta'],
         'dynamics_str': config['dynamics_str'],
-        'dynamics_params': dynamics_params
+        'dynamics_params': mixture_model_results['dynamics_params']
     },
     'feature_prior_params': {
         'centroids_prior_cov_prefactor': config['centroids_prior_cov_prefactor']
@@ -95,16 +87,20 @@ gen_model_params = {
     }
 }
 
-observations_times = ames_housing_data['observations']
-observations = ames_housing_data['observations'].copy()
-
 inference_alg_results = rncrp.helpers.run.run_inference_alg(
     inference_alg_str=config['inference_alg_str'],
-    observations=observations,
-    observations_times=observations_times,
+    observations=mixture_model_results['observations'],
+    observations_times=mixture_model_results['observations_times'],
     gen_model_params=gen_model_params,
 )
 
+scores, map_cluster_assignments = rncrp.metrics.compute_predicted_clusters_scores(
+    cluster_assignment_posteriors=inference_alg_results['cluster_assignment_posteriors'],
+    true_cluster_assignments=mixture_model_results['cluster_assignments'],
+)
+inference_alg_results.update(scores)
+inference_alg_results['map_cluster_assignments'] = map_cluster_assignments
+wandb.log(scores, step=0)
 
 sum_sqrd_distances = rncrp.metrics.compute_sum_of_squared_distances_to_nearest_center(
     X=mixture_model_results['observations'],
@@ -116,7 +112,7 @@ wandb.log({'training_reconstruction_error': sum_sqrd_distances}, step=0)
 data_to_store = dict(
     config=dict(config),  # Need to convert WandB config to proper dict
     inference_alg_results=inference_alg_results,
-    )
+    scores=scores)
 
 joblib.dump(data_to_store,
             filename=inf_alg_results_path)
