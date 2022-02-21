d53fc6a76a7ac50da24d33d374239cc36143a457
Gabrielle 'Kaili' Liu <pybeebee@gmail.com>
2022-02-20 22:23:14 -0500

    Implemented cluster ratio plots (untested)

---
+++ 08_mixture_of_dirichlet_multinomial/run_one.py
@@ -0,0 +1,113 @@
+"""
+Perform inference in a synthetic mixture of von Mises-Fisher for the specified inference
+algorithm and model parameters.
+
+Example usage:
+
+03_mixture_of_vonmises_fisher/run_one.py
+"""
+
+import joblib
+import numpy as np
+import os
+import wandb
+
+# import plot
+import rncrp.data.synthetic
+import rncrp.helpers.dynamics
+import rncrp.helpers.run
+import rncrp.metrics
+
+config_defaults = {
+    'inference_alg_str': 'Dynamical-CRP',
+    'dynamics_str': 'hyperbolic',
+    'dynamics_a': 1.,
+    'dynamics_b': 1.,
+    'dynamics_c': 1.,
+    'dynamics_omega': np.pi / 2.,
+    'n_samples': 27,
+    'n_features': 10,
+    'n_clusters': 40,
+    'alpha': 0.1,
+    'beta': 0.,
+    'likelihood_kappa': 1.,
+    'repeat_idx': 0,
+}
+
+wandb.init(project='dcrp-mixture-of-vonmises-fisher',
+           config=config_defaults)
+config = wandb.config
+
+print(f'Running:')
+for key, value in config.items():
+    print(key, ' : ', value)
+
+# determine paths
+exp_dir = '03_mixture_of_vonmises_fisher'
+results_dir_path = os.path.join(exp_dir, 'results')
+os.makedirs(results_dir_path, exist_ok=True)
+inf_alg_results_path = os.path.join(results_dir_path,
+                                    f'id={wandb.run.id}.joblib')
+wandb.log({'inf_alg_results_path': inf_alg_results_path},
+          step=0)
+
+# set seeds
+rncrp.helpers.run.set_seed(seed=config['repeat_idx'])
+
+mixture_model_results = rncrp.data.synthetic.sample_mixture_model(
+    num_obs=config['n_samples'],
+    obs_dim=config['n_features'],
+    mixing_prior_str='rncrp',
+    mixing_distribution_params={'alpha': config['alpha'],
+                                'beta': config['beta'],
+                                'dynamics_str': config['dynamics_str']},
+    component_prior_str='vonmises_fisher',
+    component_prior_params={'likelihood_kappa': config['likelihood_kappa']})
+
+gen_model_params = {
+    'mixing_params': {
+        'alpha': config['alpha'],
+        'beta': config['beta'],
+        'dynamics_str': config['dynamics_str'],
+        'dynamics_params': mixture_model_results['dynamics_params']
+    },
+    'component_prior_params': {
+        # 'centroids_prior_cov_prefactor': config['centroids_prior_cov_prefactor']
+    },
+    'likelihood_params': {
+        'distribution': 'vonmises_fisher',
+        'likelihood_kappa': config['likelihood_kappa']
+    }
+}
+
+inference_alg_results = rncrp.helpers.run.run_inference_alg(
+    inference_alg_str=config['inference_alg_str'],
+    observations=mixture_model_results['observations'],
+    observations_times=mixture_model_results['observations_times'],
+    gen_model_params=gen_model_params,
+)
+
+scores, map_cluster_assignments = rncrp.metrics.compute_predicted_clusters_scores(
+    cluster_assignment_posteriors=inference_alg_results['cluster_assignment_posteriors'],
+    true_cluster_assignments=mixture_model_results['cluster_assignments'],
+)
+inference_alg_results.update(scores)
+inference_alg_results['map_cluster_assignments'] = map_cluster_assignments
+wandb.log(scores, step=0)
+
+sum_sqrd_distances = rncrp.metrics.compute_sum_of_squared_distances_to_nearest_center(
+    X=mixture_model_results['observations'],
+    centroids=inference_alg_results['inference_alg'].centroids_after_last_obs())
+inference_alg_results['training_reconstruction_error'] = sum_sqrd_distances
+wandb.log({'training_reconstruction_error': sum_sqrd_distances}, step=0)
+
+
+data_to_store = dict(
+    config=dict(config),  # Need to convert WandB config to proper dict
+    inference_alg_results=inference_alg_results,
+    scores=scores)
+
+joblib.dump(data_to_store,
+            filename=inf_alg_results_path)
+
+print('Finished run.')
---
+++ 08_mixture_of_dirichlet_multinomial/run_sweep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p fiete
+#SBATCH -n 1                    # two cores
+#SBATCH --mem=1G                # RAM
+#SBATCH --time=99:99:99         # total run time limit (HH:MM:SS)
+#SBATCH --mail-user=rylansch
+#SBATCH --mail-type=FAIL
+
+
+# Run this, then pipe sweep ID to each individual run
+# export WANDB_CONFIG_DIR=/om2/user/rylansch
+# export WANDB_API_KEY=51a0a43a1b4ba9981701d60c5f6887cd5bf9e03e
+# source rncrp_venv/bin/activate
+# wandb sweep 03_mixture_of_vonmises_fisher/sweep_complete.yaml
+
+for i in {1..5}
+do
+  sbatch 03_mixture_of_vonmises_fisher/run_one.sh simx0pgt
+  sleep 5
+done
