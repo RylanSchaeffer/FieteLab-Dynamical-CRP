6206b0a99256765a19d6adc93bee0c465d1d79ac
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-02-20 20:45:03 -0800

    fixed incorrect plotting path

--- 03_mixture_of_vonmises_fisher/plot_mixture_of_vonmises_fisher.py
+++ 03_mixture_of_vonmises_fisher/plot_mixture_of_vonmises_fisher.py
@@ -30,7 +30,7 @@ def plot_analyze_all_inf_algs_results(all_inf_algs_results_df: pd.DataFrame,
         for plot_fn in plot_fns:
             # try:
             plot_fn(sweep_results_df=sweep_subset_results_df,
-                    plot_dir=plot_dir)
+                    plot_dir=sweep_dynamics_str_dir)
             # except Exception as e:
             #     print(f'Exception: {e}')
 
