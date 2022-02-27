fc793d1f4cfbf8125c572030110311d2818deef8
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-02-27 14:03:12 -0800

    Added plotting for SwAV pretrained embeddings

--- 05_swav_pretrained/analyze_sweep.py
+++ 05_swav_pretrained/analyze_sweep.py
@@ -2,7 +2,8 @@ import numpy as np
 import os
 import pandas as pd
 
-from rncrp.helpers.analyze import download_wandb_project_runs_results, generate_and_save_cluster_ratio_data
+from rncrp.helpers.analyze import download_wandb_project_runs_results,\
+    generate_and_save_cluster_ratio_data
 import plot_swav_pretrained
 
 exp_dir = '05_swav_pretrained'
@@ -10,9 +11,11 @@ results_dir = os.path.join(exp_dir, 'results')
 os.makedirs(results_dir, exist_ok=True)
 wandb_sweep_path = "rylan/dcrp-swav-pretrained"
 sweep_name = 'bia0srmy'
-sweep_dir = os.path.join(results_dir, sweep_name)
-os.makedirs(sweep_dir, exist_ok=True)
-sweep_results_df_path = os.path.join(sweep_dir, f'sweep={sweep_name}_results.csv')
+sweep_results_dir_path = os.path.join(results_dir, sweep_name)
+os.makedirs(sweep_results_dir_path, exist_ok=True)
+sweep_results_df_path = os.path.join(
+    sweep_results_dir_path,
+    f'sweep={sweep_name}_results.csv')
 
 if not os.path.isfile(sweep_results_df_path):
     sweep_results_df = download_wandb_project_runs_results(
@@ -29,13 +32,42 @@ else:
 
 print(f"Number of runs: {sweep_results_df.shape[0]} for sweep={sweep_name}")
 
-# Generate data for cluster ratio plots
-generate_and_save_cluster_ratio_data(all_inf_algs_results_df=sweep_results_df,
-                                     plot_dir=sweep_dir)
+cluster_ratio_dfs_results = generate_and_save_cluster_ratio_data(
+    all_inf_algs_results_df=sweep_results_df,
+    sweep_results_dir_path=sweep_results_dir_path)
+
+# Join cluster ratio dataframes with sweep hyperparameters
+#     Dataframes will have columns:
+#         1) 'inf_alg_results_path'
+#         2) 'alpha'
+#         3) 'n_features'
+#         4) 'snr'
+#         5) 'dynamics_str',
+#         6) 'inference_alg_str'
+#         7-on) '0', '1', ..., 'max number of observations.'
+num_inferred_clusters_div_num_true_clusters_by_obs_idx_df = pd.merge(
+    left=sweep_results_df[['inf_alg_results_path', 'alpha', 'n_features', 'snr', 'dynamics_str', 'inference_alg_str']],
+    right=cluster_ratio_dfs_results['num_inferred_clusters_div_num_true_clusters_by_obs_idx_df'],
+    how='inner',
+    on='inf_alg_results_path')
+num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df = pd.merge(
+    left=sweep_results_df[['inf_alg_results_path', 'alpha', 'n_features', 'snr', 'dynamics_str', 'inference_alg_str']],
+    right=cluster_ratio_dfs_results['num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df'],
+    how='inner',
+    on='inf_alg_results_path')
+num_true_clusters_div_total_num_true_clusters_by_obs_idx_df = pd.merge(
+    left=sweep_results_df[['inf_alg_results_path', 'alpha', 'n_features', 'snr', 'dynamics_str', 'inference_alg_str']],
+    right=cluster_ratio_dfs_results['num_true_clusters_div_total_num_true_clusters_by_obs_idx_df'],
+    how='inner',
+    on='inf_alg_results_path')
+
 
 plot_swav_pretrained.plot_analyze_all_inf_algs_results(
     all_inf_algs_results_df=sweep_results_df,
-    plot_dir=sweep_dir,
+    num_inferred_clusters_div_num_true_clusters_by_obs_idx_df=num_inferred_clusters_div_num_true_clusters_by_obs_idx_df,
+    num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df=num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df,
+    num_true_clusters_div_total_num_true_clusters_by_obs_idx_df=num_true_clusters_div_total_num_true_clusters_by_obs_idx_df,
+    plot_dir=sweep_results_dir_path,
 )
 
 print(f'Finished 05_swav_pretrained/plot_sweep.py for sweep={sweep_name}.')
--- 05_swav_pretrained/plot_swav_pretrained.py
+++ 05_swav_pretrained/plot_swav_pretrained.py
@@ -9,11 +9,62 @@ import rncrp.plot.plot_general
 
 
 def plot_analyze_all_inf_algs_results(all_inf_algs_results_df: pd.DataFrame,
+                                      num_inferred_clusters_div_num_true_clusters_by_obs_idx_df: pd.DataFrame,
+                                      num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df: pd.DataFrame,
+                                      num_true_clusters_div_total_num_true_clusters_by_obs_idx_df: pd.DataFrame,
                                       plot_dir: str):
+
     for dynamics_str, sweep_subset_results_df in all_inf_algs_results_df.groupby('dynamics_str'):
         sweep_dynamics_str_dir = os.path.join(plot_dir, dynamics_str)
         os.makedirs(sweep_dynamics_str_dir, exist_ok=True)
         print(f'Plotting dynamics {dynamics_str}')
+
+        if dynamics_str == 'step':
+            title_str = r'$\Theta(\Delta)$'
+        elif dynamics_str == 'exp':
+            title_str = r'$\exp(-\Delta)$'
+        elif dynamics_str == 'sinusoid':
+            title_str = r'$\cos(\Delta)$'
+        elif dynamics_str == 'hyperbolic':
+            title_str = r'$\frac{1}{1 + \Delta}$'
+        else:
+            title_str = None
+
+        ratio_dfs_and_plot_fns = [
+            (num_inferred_clusters_div_num_true_clusters_by_obs_idx_df,
+             rncrp.plot.plot_general.plot_num_inferred_clusters_div_num_true_clusters_by_obs_idx),
+            (num_inferred_clusters_div_total_num_true_clusters_by_obs_idx_df,
+             rncrp.plot.plot_general.plot_num_inferred_clusters_div_total_num_true_clusters_by_obs_idx),
+            (num_true_clusters_div_total_num_true_clusters_by_obs_idx_df,
+             rncrp.plot.plot_general.plot_num_true_clusters_div_total_num_true_clusters_by_obs_idx),
+        ]
+
+        for ratio_df, ratio_plot_fn in ratio_dfs_and_plot_fns:
+
+            ratio_df = ratio_df[
+                (ratio_df['inference_alg_str'] == 'Dynamical-CRP') &
+                (ratio_df['dynamics_str'] == dynamics_str)]
+
+            ratio_df = pd.melt(ratio_df,
+                               id_vars=['inf_alg_results_path',
+                                        'alpha',
+                                        'n_features',
+                                        'snr',
+                                        'inference_alg_str',
+                                        'dynamics_str'],
+                               var_name='obs_idx',
+                               value_name='cluster_ratio')
+
+            # Make sure that observation index is recognized as an integer
+            ratio_df['obs_idx'] = pd.to_numeric(ratio_df['obs_idx'])
+
+            ratio_plot_fn(
+                ratio_df=ratio_df,
+                plot_dir=sweep_dynamics_str_dir)
+
+            plt.close('all')
+
+            print(f'Plotted {ratio_plot_fn}')
 
         plot_fns = [
             plot_cluster_multiclass_classification_score_by_alpha_cross_kappa,
