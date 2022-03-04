0e5ca33b44198a64a235b509669572228fc8ac12
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-03-04 08:50:20 -0800

    Adding CGS to python package

--- rncrp/inference/__init__.py
+++ rncrp/inference/__init__.py
@@ -1,4 +1,5 @@
 from rncrp.inference.base import BaseModel
+from rncrp.inference.collapsed_gibbs import CollapsedGibbsSampler
 from rncrp.inference.dpmeans import DPMeans
 from rncrp.inference.dcrp import DynamicalCRP
 from rncrp.inference.kmeans import KMeansWrapper
