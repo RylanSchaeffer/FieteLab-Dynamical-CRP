06ec36dc45f065c2ab8106dc6a04c35a405a761e
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-03-08 12:12:36 -0800

    Fixed KMeans import

--- rncrp/inference/__init__.py
+++ rncrp/inference/__init__.py
@@ -3,6 +3,6 @@ from rncrp.inference.collapsed_gibbs import CollapsedGibbsSampler
 from rncrp.inference.collapsed_gibbs_new import CollapsedGibbsSamplerNew
 from rncrp.inference.dpmeans import DPMeans
 from rncrp.inference.dcrp import DynamicalCRP
-from rncrp.inference.kmeans import KMeansWrapper
+from rncrp.inference.kmeans import KMeans
 from rncrp.inference.rcrp import RecursiveCRP
 from rncrp.inference.vigmm import VariationalInferenceGMM
